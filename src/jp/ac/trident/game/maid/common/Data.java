735985df1491c98b669d28af91ee73ea548fb24a
Ryugo Abe <abe.ryugo@gmail.com>
2013-12-19 13:19:21 +0900

    DB繋げるライブラリ

---
+++ src/jp/ac/trident/game/maid/common/Data.java
@@ -0,0 +1,39 @@
+package jp.ac.trident.game.maid.common;
+
+public class Data {
+	  protected int id;	// アイテムの管理番号
+	  protected String name; // アイテムの名前(画像ファイル名と統一)
+	  protected float x; // アイテムのx座標
+	  protected float y; // アイテムのy座標
+	  protected float rotate; // アイテムの回転
+
+
+	  public Data(int id, String name, float x,float y,float rotate){
+	    this.id = id;
+	    this.name = name;
+	    this.x = x;
+	    this.y = y;
+	    this.rotate = rotate;
+
+	  }
+
+	  public String getName(){
+	    return name;
+	  }
+
+	  public float getX(){
+	    return x;
+	  }
+	  public float getY(){
+		    return y;
+	  }
+
+	  public float getRotate(){
+		    return rotate;
+	  }
+
+	  public int getId(){
+	    return id;
+	  }
+
+	}
